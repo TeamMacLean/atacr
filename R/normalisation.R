367a99e7a425f1d676beae0b27ea06dfc0d72919
danmaclean <maclean.daniel@gmail.com>
2017-02-03 17:05:08 +0000

    added by treatment normalisation

--- R/normalisation.R
+++ R/normalisation.R
@@ -2,22 +2,47 @@
 #'  @export
 #'  @param data a list of SummarizedExperiment objects from atacr::make_counts()
 #' @param which the subdivision of the genome to calculate correlations either 'whole_genome', 'bait_windows' or 'non_bait_windows'
+#' @param by_treatment (FALSE) will group the assay into different treatments and normalise each separately - assumes that within treatment groups the samples should show little difference, but between sample treatment groups could show lots of difference between windows.
 #' @return a SummarizedExperiment object with a new, normalised assay matrix
-library_size_normalisation <- function(data, which = "bait_windows"){
+library_size_normalisation <- function(data, which = "bait_windows", by_treatment = FALSE ){
+    l <- list()
+    d <- data[[which]]
+    if ( !by_treatment ){
+      return( library_size_normalisation_internal( data[[which]] ) )
+    }
+    else {
+      for (treatment in unique(data$treatments) ){
+        samples <- names_from_treatment(data, treatment)
+        treat_norm <- library_size_normalisation_internal( data[[which]][,samples] )
+        l[[treatment]] <- SummarizedExperiment::assay( treat_norm )
+      }
+        full_mat <- do.call(cbind, l)
+        SummarizedExperiment::assay(d) <- full_mat
+        return( d )
+    }
 
-  scaling_factors <- library_size_scaling_factors(data, which)
-  normalised_sample_matrix <- scale_normalise(SummarizedExperiment::assay( data[[which]]), scaling_factors)
-  se_copy <- data[[which]]
+
+}
+
+names_from_treatment <- function(data, treatment){
+  return(data$sample_names[which(data$treatments == treatment)] )
+}
+#' do a library size normalisation
+#' @param se a SummarizedExperiment object such as 'bait_windows' from atacr::make_counts()
+library_size_normalisation_internal <- function(se){
+
+  scaling_factors <- library_size_scaling_factors( se )
+  normalised_sample_matrix <- scale_normalise(SummarizedExperiment::assay( se ), scaling_factors)
+  se_copy <- se
   SummarizedExperiment::assay(se_copy) <- normalised_sample_matrix
   return(se_copy)
 }
 
 #' calculate scaling factors for library size
 #' @export
-#' @param data a list of SummarizedExperiment objects from atacr::make_counts()
-#' @param which the subdivision of the genome to calculate correlations either 'whole_genome', 'bait_windows' or 'non_bait_windows'
-library_size_scaling_factors <- function(data, which = "bait_windows"){
-  sample_matrix <- SummarizedExperiment::assay( data[[which]] )
+#' @param se a SummarizedExperiment object such as 'bait_windows' from atacr::make_counts()
+library_size_scaling_factors <- function( se ){
+  sample_matrix <- SummarizedExperiment::assay( se )
   return(get_scaling_factors(sample_matrix))
 }
 
@@ -37,27 +62,46 @@ scale_normalise <- function( sample_matrix, scaling_factors){
 #' @param data a list of SummarizedExperiment objects from atacr::make_counts()
 #' @param which the subdivision of the genome to calculate correlations either 'whole_genome', 'bait_windows' or 'non_bait_windows'
 #' @param window_file a text file containing the positions of control window/gene ranges
+#' @param by_treatment (FALSE) will group the assay into different treatments and normalise each separately - assumes that within treatment groups the samples should show little difference, but between sample treatment groups could show lots of difference between windows.
 #' @return a vector of scaling factors from control genes
-control_window_scaling_factors <- function( data, window_file, which = "bait_windows" ){
+control_window_scaling_factors <- function( se, window_file){
   control_window_regions <- get_bait_regions_from_text( window_file )
-  keep <- IRanges::overlapsAny( SummarizedExperiment::rowRanges( data[[which]] ), control_window_regions )
-
-  control_windows <- data[[which]][keep, ]
+  keep <- IRanges::overlapsAny( SummarizedExperiment::rowRanges( se ), control_window_regions )
+  control_windows <- se[keep, ]
   sample_matrix <- SummarizedExperiment::assay( control_windows )
   return(get_scaling_factors(sample_matrix))
 
 }
+#' do a control window scaling normalisation
+#' @param se a SummarizedExperiment object such as 'bait_windows' from atacr::make_counts()
+control_window_normalise_internal <- function( se, window_file ){
+  scaling_factors <- control_window_scaling_factors( se, window_file)
+  normalised_sample_matrix <- scale_normalise(SummarizedExperiment::assay( se ), scaling_factors)
+  se_copy <- se
+  SummarizedExperiment::assay(se_copy) <- normalised_sample_matrix
+  return(se_copy)
 
+}
 #' performs control window based normalisation of the selected set of windows, calculates a median virtual experiment and normalises to that
 #' @export
 #' @param data a list of SummarizedExperiment objects from atacr::make_counts()
 #' @param which the subdivision of the genome to calculate correlations either 'whole_genome', 'bait_windows' or 'non_bait_windows'
 #' @param window_file a text file containing the positions of control window/gene ranges
 #' @return a vector of scaling factors from control genes
-control_window_normalise <- function(data, window_file, which = "bait_windows"){
-  scaling_factors <- control_window_scaling_factors(data, window_file, which = which)
-  normalised_sample_matrix <- scale_normalise(SummarizedExperiment::assay( data[[which]]), scaling_factors)
-  se_copy <- data[[which]]
-  SummarizedExperiment::assay(se_copy) <- normalised_sample_matrix
-  return(se_copy)
+control_window_normalise <- function(data, window_file, which = "bait_windows", by_treatment = FALSE ){
+    d <- data[[which]]
+    l <- list()
+    if(!by_treatment){
+      return( control_window_normalise_internal(data[[which]], window_file ))
+    }
+    else{
+      for (treatment in unique(data$treatments) ){
+        samples <- names_from_treatment(data, treatment)
+        treat_norm <- control_window_normalise_internal( data[[which]][,samples], window_file )
+        l[[treatment]] <- SummarizedExperiment::assay( treat_norm )
+      }
+      full_mat <- do.call(cbind, l)
+      SummarizedExperiment::assay(d) <- full_mat
+      return( d )
+    }
 }
