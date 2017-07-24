f52aa5d1ae0918e3ad6218a56b15ca3e3c872868
danmaclean <maclean.daniel@gmail.com>
2017-07-24 11:16:32 +0100

    remove order

--- R/figures.R
+++ R/figures.R
@@ -26,14 +26,6 @@ get_heatmap <- function(counts, method="pearson", namesort=TRUE){
   df <- expand.grid(sample_1=colnames(counts), sample_2=colnames(counts))
   df <- data.frame(sample_1=df$sample_1, sample_2=df$sample_2, correlation=cors)
 
-  if (namesort){
-   df$sample_1 <- as.character(data$sample_1)
-   df$sample_1 <- factor(df$sample_1, levels=df$sample_1[order(df$sample_1)])
-   df$sample_2 <- as.character(data$sample_2)
-   df$sample_2 <- factor(df$sample_2, levels=df$sample_2[order(df$sample_2)])
-
-  }
-
   correlation <- NULL #deal with devtools::check()
   heatmap <- ggplot2::ggplot(df, ggplot2::aes(sample_1, sample_2, fill = correlation)) + ggplot2::geom_raster()  + ggthemes::scale_color_ptol() + ggplot2::theme_minimal() + ggplot2::ggtitle("Sample Similarities") + ggplot2::labs(x= NULL, y=NULL)
 
