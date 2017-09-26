6f98bdf2e402ad546ba73f380fbd9bd9370cd2a4
danmaclean <maclean.daniel@gmail.com>
2017-09-26 11:16:03 +0100

    add scale factor

--- R/normalisation.R
+++ R/normalisation.R
@@ -164,6 +164,20 @@ scale_normalise <- function( sample_matrix, scaling_factors){ #nocov start
   return(scaled)
 } #nocov end
 
+#' normalise by a provided set of scaling factors
+#' @export
+#' @param data a list of SummarizedExperiment objects from atacr::make_counts()
+#' @param which the subdivision of the genome to calculate correlations either 'whole_genome', 'bait_windows' or 'non_bait_windows'
+#' @return a SummarizedExperiment with scale normalised window values
+scale_factor_normalise <- function(data, which = "bait_windows", scaling_factors = NULL){
+  se = data[[which]]
+  normalised_sample_matrix <- scale_normalise(SummarizedExperiment::assay( se ), scaling_factors)
+  se_copy <- se
+  SummarizedExperiment::assay(se_copy) <- normalised_sample_matrix
+  return(se_copy)
+}
+
+
 #' extract scaling factors from control windows (often from a file of control gene positions)
 #' @export
 #' @param se a SummarizedExperiment object
